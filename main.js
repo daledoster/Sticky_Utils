b1e4c0d64e2218538fe93144c7eb95076ceb09b7
daledoster <dale.doster1@marist.edu>
2020-05-15 23:56:15 -0400

    Added more user friendly functionality to navbar spacing and input

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ function createWindow() {
   let win = new BrowserWindow({
     width: 400,
     height: 650,
+    'minWidth': 400,
+    'minHeight': 400,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
+  win.webContents.openDevTools();
   win.setMenu(null);
   win.loadFile('./src/views/index.html');
 }
